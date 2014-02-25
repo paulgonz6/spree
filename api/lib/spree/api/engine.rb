f5fee1093ae9aca35ad17d8fc8c834881e0ef64d
Richard Nuno <me@richardnuno.com>
2014-02-25 10:37:36 -0500

    Bump versioncake to ~> 2.0.0
    Updated configuration with the namespaced and renamed 2.0 options.

--- api/lib/spree/api/engine.rb
+++ api/lib/spree/api/engine.rb
@@ -1,4 +1,5 @@
 require 'rails/engine'
+require 'versioncake'
 
 module Spree
   module Api
@@ -11,8 +12,8 @@ module Spree
         config.include_child_root = false
       end
 
-      config.view_versions = [1]
-      config.view_version_extraction_strategy = :http_header
+      config.versioncake.supported_version_numbers = [1]
+      config.versioncake.extraction_strategy = :http_header
 
       initializer "spree.api.environment", :before => :load_config_initializers do |app|
         Spree::Api::Config = Spree::ApiConfiguration.new
