02be557bd88164faf0446b1c5f375ae5991a228d
Jordan Brough <git.j@brgh.net>
2014-08-05 15:32:15 -0600

    wip

--- core/app/models/spree/reimbursement_type.rb
+++ core/app/models/spree/reimbursement_type.rb
@@ -2,6 +2,8 @@ module Spree
   class ReimbursementType < Spree::Base
     include Spree::NamedType
 
+    REFUND = 'refund'
+
     has_many :return_items
   end
 end
--- core/db/migrate/20140731150017_create_spree_reimbursement_types.rb
+++ core/db/migrate/20140731150017_create_spree_reimbursement_types.rb
@@ -8,6 +8,8 @@ class CreateSpreeReimbursementTypes < ActiveRecord::Migration
       t.timestamps
     end
 
+    Spree::ReimbursementType.create!(name: Spree::ReimbursementType::REFUND)
+
     add_column :spree_return_items, :preferred_reimbursement_type_id, :integer
     add_index :spree_return_items, :preferred_reimbursement_type_id, name: 'index_return_items_on_preferred_reimbursement_type_id'
 
