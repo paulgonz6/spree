7e6960ea24170d328de09fecbc633c8ad2d109c4
Jordan Brough <git.j@brgh.net>
2014-08-05 18:22:23 -0600

    wip

--- backend/app/controllers/spree/admin/customer_returns_controller.rb
+++ backend/app/controllers/spree/admin/customer_returns_controller.rb
@@ -1,77 +1,74 @@
 module Spree
   module Admin
-    class CustomerReturnsController < Spree::Admin::BaseController
-      before_filter :load_order
+    class CustomerReturnsController < ResourceController
+      belongs_to 'spree/order', find_by: :number
+
+      before_filter :parent # ensure order gets loaded to support our pseudo parent-child relationship
       before_filter :load_form_data, only: [:new, :edit]
 
-      def index
-        order_return_items = Spree::ReturnItem.accessible_by(current_ability, :read).where(inventory_unit_id: @order.inventory_units.pluck(:id))
-        order_customer_returns = order_return_items.map(&:customer_return).compact.uniq
-        @customer_returns = Kaminari.paginate_array(order_customer_returns).page(params[:page]).per(params[:per_page] || Spree::Config[:customer_returns_per_page])
-      end
+      create.before :build_return_items_from_params
+      create.fails  :load_form_data
 
-      def new
-        @customer_return = Spree::CustomerReturn.new
-      end
-
-      def show
-        @customer_return = Spree::CustomerReturn.accessible_by(current_ability, :read).find(params[:id])
+      def edit
         @pending_return_items = @customer_return.return_items.pending
         @accepted_return_items = @customer_return.return_items.accepted
         @rejected_return_items = @customer_return.return_items.rejected
         @manual_intervention_return_items = @customer_return.return_items.manual_intervention_required
-      end
-
-      def create
-        @customer_return = Spree::CustomerReturn.new(stock_location_id: customer_return_params[:stock_location_id])
-        @customer_return.return_items = build_return_items_from_params
-
-        if @customer_return.save
-          flash[:success] = flash_message_for(@customer_return, :successfully_created)
-          redirect_to admin_order_customer_return_path(@order, @customer_return)
-        else
-          flash[:error] = Spree.t(:could_not_create_customer_return)
-          load_form_data
-          render action: "new"
-        end
-
+        super
       end
 
       private
 
-        def load_order
-          @order = Spree::Order.accessible_by(current_ability, :read).find_by(number: params[:order_id])
-        end
+      def location_after_save
+        url_for([:edit, :admin, @order, @customer_return])
+      end
 
-        def load_form_data
-          return_items_by_rma_id = @order.inventory_units.map(&:current_or_new_return_item).group_by(&:return_authorization_id)
-          @new_return_items = filter_return_items_with_customer_returns(return_items_by_rma_id.delete(nil))
-          @rma_return_items = filter_return_items_with_customer_returns(return_items_by_rma_id.values.flatten)
+      def build_resource
+        Spree::CustomerReturn.new
+      end
 
-          @reimbursement_types = Spree::ReimbursementType.accessible_by(current_ability, :read).active
+      def find_resource
+        Spree::CustomerReturn.accessible_by(current_ability, :read).find(params[:id])
+      end
 
-          @allow_amount_edit = can?(:manage, Spree::CustomerReturn)
-        end
+      def collection
+        parent # trigger loading the order
+        @collection ||= Spree::ReturnItem
+          .accessible_by(current_ability, :read)
+          .where(inventory_unit_id: @order.inventory_units.pluck(:id))
+          .map(&:customer_return).uniq.compact
+        @customer_returns = @collection
+      end
 
-        def customer_return_params
-          params.require('customer_return').permit(permitted_customer_return_attributes)
-        end
+      def load_form_data
+        return_items_by_rma_id = @order.inventory_units.map(&:current_or_new_return_item).group_by(&:return_authorization_id)
+        @new_return_items = filter_return_items_with_customer_returns(return_items_by_rma_id.delete(nil))
+        @rma_return_items = filter_return_items_with_customer_returns(return_items_by_rma_id.values.flatten)
 
-        def filter_return_items_with_customer_returns(return_items)
-          return [] unless return_items
-          return_items.select { |return_item| return_item.customer_return.nil? }
-        end
+        @reimbursement_types = Spree::ReimbursementType.accessible_by(current_ability, :read).active
 
-        def build_return_items_from_params
-          return_items_params = customer_return_params[:return_items_attributes].values
+        @allow_amount_edit = can?(:manage, Spree::CustomerReturn)
+      end
 
-          return_items_params.map do |item_params|
-            next unless item_params.delete('returned') == '1'
-            return_item = Spree::ReturnItem.find_or_initialize_by(id: item_params[:id])
-            return_item.assign_attributes(item_params)
-            return_item
-          end.compact
-        end
+      def customer_return_params
+        params.require('customer_return').permit(permitted_customer_return_attributes)
+      end
+
+      def filter_return_items_with_customer_returns(return_items)
+        return [] unless return_items
+        return_items.select { |return_item| return_item.customer_return.nil? }
+      end
+
+      def build_return_items_from_params
+        return_items_params = customer_return_params.delete(:return_items_attributes).values
+
+        @customer_return.return_items = return_items_params.map do |item_params|
+          next unless item_params.delete('returned') == '1'
+          return_item = Spree::ReturnItem.find_or_initialize_by(id: item_params[:id])
+          return_item.assign_attributes(item_params)
+          return_item
+        end.compact
+      end
 
     end
   end
