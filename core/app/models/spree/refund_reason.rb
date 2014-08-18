badogar/solidus

Introduce refund and return authorization reason.
Fixes #5003

core/app/models/spree/refund_reason.rb
@@ -1,0 +1,13 @@
+module Spree
+  class RefundReason < Spree::Base
+    include Spree::ReasonType
+
+    RETURN_PROCESSING_REASON = 'Return processing'
+
+    has_many :refunds
+
+    def self.return_processing_reason
+      find_by!(name: RETURN_PROCESSING_REASON, mutable: false)
+    end
+  end
+end

paulgonz6/spree

Conflicts:
	backend/app/views/spree/admin/return_authorizations/_form.html.erb
	core/app/models/spree/return_authorization.rb
	core/spec/models/spree/return_authorization_spec.rb

core/app/models/spree/refund_reason.rb
@@ -1,0 +1,13 @@
+module Spree
+  class RefundReason < ActiveRecord::Base
+    include Spree::ReasonType
+
+    RETURN_PROCESSING_REASON = 'Return processing'
+
+    has_many :refunds
+
+    def self.return_processing_reason
+      find_by!(name: RETURN_PROCESSING_REASON, mutable: false)
+    end
+  end
+end