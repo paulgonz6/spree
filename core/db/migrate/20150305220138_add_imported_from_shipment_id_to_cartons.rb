528b9eaf5c94890aac1387799213336937d2fa6b
Jordan Brough <git.j@brgh.net>
2015-03-06 11:19:17 -0700

    Add unique index to temporary db field
    To ensure we never import more than one carton per shipment

--- core/db/migrate/20150305220138_add_imported_from_shipment_id_to_cartons.rb
+++ core/db/migrate/20150305220138_add_imported_from_shipment_id_to_cartons.rb
@@ -3,6 +3,6 @@ class AddImportedFromShipmentIdToCartons < ActiveRecord::Migration
     # Temporarily add this column until we're sure that this migration and the
     # upcoming code changes are working correctly
     add_column :spree_cartons, :imported_from_shipment_id, :integer
-    add_index :spree_cartons, :imported_from_shipment_id
+    add_index :spree_cartons, :imported_from_shipment_id, unique: true
   end
 end
