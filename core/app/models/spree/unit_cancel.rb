4f7b716c4ab9409d9ebcf0141fbb5a9764e7f693
Jordan Brough <git.j@brgh.net>
2015-04-09 08:50:22 -0600

    Exchanges fix

--- core/app/models/spree/unit_cancel.rb
+++ core/app/models/spree/unit_cancel.rb
@@ -27,6 +27,6 @@ class Spree::UnitCancel < ActiveRecord::Base
   # This method is used by Adjustment#update to recalculate the cost.
   def compute_amount(line_item)
     raise "Adjustable does not match line item" unless line_item == inventory_unit.line_item
-    -(inventory_unit.line_item.total.to_d / inventory_unit.line_item.inventory_units.not_canceled.size)
+    -(line_item.total.to_d / line_item.inventory_units.not_canceled.reject(&:original_return_item ).size)
   end
 end
